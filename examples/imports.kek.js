b0849a66f8d4c35877b863f82dff3710b05d5aea
pxpcandy <39356292+pxpcandy@users.noreply.github.com>
2021-06-14 14:00:57 -0500

    Delete imports.kek.js

--- examples/imports.kek.js
+++
@@ -1,7 +0,0 @@
-/* Made with KekkerScript | https://github.com/pxpcandy/kekkerscript */
-
-/*
-	 Import path module and resolve the current path 
-*/
-const resolve = require('path').resolve;
-console.log(resolve('.'));
